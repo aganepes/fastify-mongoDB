0d0baa6a680308f8cb99d9d5d6dd011872d8681c
aganepesAI <aganepesmath@gmail.com>
2025-09-19 19:24:28 +0500

    unnecessary clear

--- src/plugins/authentication.ts
+++ src/plugins/authentication.ts
@@ -3,7 +3,6 @@ import  FastifyPlugin from 'fastify-plugin';
 
 async function authenticationPlugin(fastify:FastifyInstance) {
 
-  // Auth add to decorator
   fastify.decorate('authenticate', async function (request:FastifyRequest, reply:FastifyReply) {
     try {
       await request.jwtVerify();
@@ -15,43 +14,3 @@ async function authenticationPlugin(fastify:FastifyInstance) {
 }
 
 export default FastifyPlugin(authenticationPlugin);
-
-/*
-function authenticateToken(request, reply, next) {
-    const authHeader = request.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
-    
-    if (!token) {
-        return reply.status(401).send({ message: 'Erişim tokenı gerekli' });
-    }
-    
-    jwt.verify(token, ACCESS_TOKEN_SECRET, (err, user) => {
-        if (err) {
-            return reply.status(403).send({ message: 'Geçersiz token' });
-        }
-        
-        request.user = user;
-        next();
-    });
-}
-
-
-
-  fastify.decorate('adminOnly', async function (request:FastifyRequest, reply:FastifyReply) {
-    try {
-      await request.jwtVerify();
-      const user = request.user;
-      
-      // Admin control
-      if (user.role !== 'admin') {
-        return reply.status(403).send({
-          error: 'Forbidden',
-          message: 'Sadece adminler bu işlemi yapabilir'
-        });
-      }
-    } catch (err) {  
-      reply.send(err);
-    }
-  });
-
-*/ 
